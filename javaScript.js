cf272a92a9aaa2ec75152dac24fa4d102ab61762
Abdul Samad Khan <abdulsamad31650@gmail.com>
2024-01-10 11:42:01 +0500

    medel opened

--- javaScript.js
+++ javaScript.js
@@ -28,6 +28,7 @@ function display() {
 		const editSpan = document.createElement('span');
 		editSpan.className = 'editBtn';
 		editSpan.innerHTML = '&#9998;';
+
 		// delete sign to each list
 		const deleteSpan = document.createElement('span');
 		let txt = document.createTextNode('\u00D7');
@@ -46,6 +47,7 @@ function display() {
 		// console.log(li);
 	});
 	removeTodo();
+	editTodo();
 }
 
 display();
@@ -54,7 +56,7 @@ display();
 function removeTodo() {
 	let closeButtons = document.querySelectorAll('.closeBtn');
 	closeButtons.forEach(item => {
-		console.log(item.parentElement.firstChild.data);
+		// console.log(item.parentElement.firstChild.data);
 		// console.log(todos);
 		item.onclick = function () {
 			let parent = this.parentElement;
@@ -88,28 +90,72 @@ function removeTodo() {
 // 		todos.splice(remP, 1);
 // 	};
 // });
-// Get the modal
-var modal = document.getElementById('myModal');
+// // Get the modal
+// var modal = document.getElementById('myModal');
 
-// Get the button that opens the modal
-var btn = document.getElementById('myBtn');
+// // Get the button that opens the modal
+// // var btn = document.getElementById('myBtn');
+// let editbtns = document.querySelectorAll('.editBtn');
+// console.log(`Edit buttons : ${Array.from(editbtns)}`);
+// // Get the <span> element that closes the modal
+// editbtns.forEach(btn => {
+// 	btn.addEventListener('click', function () {
+// 		var span = document.getElementsByClassName('close')[0];
+// 		modal.style.display = 'block';
+// 		span.onclick = function () {
+// 			modal.style.display = 'none';
+// 		};
+// 		window.onclick = function (event) {
+// 			if (event.target == modal) {
+// 				modal.style.display = 'none';
+// 			}
+// 		};
+// 	});
+// });
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName('close')[0];
+// var span = document.getElementsByClassName('close')[0];
 
 // When the user clicks the button, open the modal
-btn.onclick = function () {
-	modal.style.display = 'block';
-};
+// btn.onclick = function () {
+// 	modal.style.display = 'block';
+// };
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-	modal.style.display = 'none';
-};
+// span.onclick = function () {
+// 	modal.style.display = 'none';
+// };
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-	if (event.target == modal) {
-		modal.style.display = 'none';
-	}
-};
+// window.onclick = function (event) {
+// 	if (event.target == modal) {
+// 		modal.style.display = 'none';
+// 	}
+// };
+function editTodo() {
+	// Edit functionality
+	// Get the modal
+	var modal = document.getElementById('myModal');
+
+	// Get the button that opens the modal
+	// var btn = document.getElementById('myBtn');
+	let editbtns = document.querySelectorAll('.editBtn');
+	console.log(editbtns);
+	// Get the <span> element that closes the modal
+	let myedit = Array.from(editbtns);
+	myedit.forEach(btn => {
+		btn.addEventListener('click', function () {
+			var span = document.getElementsByClassName('close')[0];
+			modal.style.display = 'block';
+			span.onclick = function () {
+				modal.style.display = 'none';
+			};
+			window.onclick = function (event) {
+				if (event.target == modal) {
+					modal.style.display = 'none';
+				}
+			};
+		});
+	});
+
+	// edit functionality End
+}
